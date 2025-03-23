face0ce36ea47601b15f7ceff789cef69daaa34e
Kayo Vinicius <151939515+kayotc@users.noreply.github.com>
2025-03-23 19:12:28 -0300

    Add files via upload

--- Lumatitaka/cadastro/cadastro.js
+++ Lumatitaka/cadastro/cadastro.js
@@ -1,3 +1,33 @@
 
 
+const btn = document.querySelector('button');
+
+function verificar(){
+    const inputN = document.querySelector('#name')
+    const nome = inputN.value
+
+    const inputU = document.querySelector('#user')
+    const user = inputU.value
+
+    const inputP = document.querySelector('#pass')
+    const pass = inputP.value
+
+    const inputPC = document.querySelector('#pass-confirme')
+    const passconfirme = inputPC.value
+
+    if (nome === '' || user === ''  || pass === '' || passconfirme === ''){
+        let msg = document.querySelector('#msg').textContent = "Preencha todos os campos";  
+        msg.style.color = "red";
+    } else {
+        alert('obg')
+    }
+}
+
+btn.addEventListener('click', ()=>{
+    verificar()
+})
+
+
+
+
 
